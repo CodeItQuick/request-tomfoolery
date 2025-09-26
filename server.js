b6aced0241e07288e472a2dbbec93e44f32cfaaf
CodeItQuick <evanontario@hotmail.com>
2025-09-26 08:02:46 -0300

    add vue and express app

---
+++ server.js
@@ -0,0 +1,15 @@
+﻿import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+import cors from 'cors';
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running on http://localhost:${PORT}`);
+});
--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,38 @@
   <div>
     <h1>Welcome to Vue 3!</h1>
     <p>This is your new Vue app inside the using-request project.</p>
+    <button @click="fetchMessage">Get Server Message</button>
+    <div v-if="message">Server says: {{ message }}</div>
+    <div v-if="error" style="color: red">{{ error }}</div>
   </div>
 </template>
 
 <script>
-// You can add your script logic here
+export default {
+  data() {
+    return {
+      message: '',
+      error: ''
+    };
+  },
+  methods: {
+    async fetchMessage() {
+      this.message = '';
+      this.error = '';
+      try {
+        const response = await fetch('http://localhost:3000/');
+        if (!response.ok) {
+          this.error = 'Network response was not ok';
+          return;
+        }
+        const data = await response.json();
+        this.message = data.message;
+      } catch (err) {
+        this.error = 'Request failed: ' + err.message;
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
@@ -14,4 +41,3 @@ h1 {
   color: #42b983;
 }
 </style>
-
